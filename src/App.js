5040ceca1bc388f0791f9754142f34bfefe7e52a
Rio Oktafianto <rio.oktafianto@anugrah-argon.com>
2022-04-13 09:42:42 +0700

    test

--- src/App.js
+++ src/App.js
@@ -2,21 +2,15 @@ import PropTypes from "prop-types";
 import './App.css';
 import Shapes from './components/shapes/shapes'
 import Logos from './components/logos/logos'
-import {ReactComponent as MakeLogo} from './logo.svg'
-import {ReactComponent as ReactLogo} from './react-logo.svg'
+import { ReactComponent as MakeLogo } from './logo.svg'
+import { ReactComponent as ReactLogo } from './react-logo.svg'
 
 function App({ headline, showLogos, backgroundImage }) {
   return (
     <div className="App" style={{ backgroundImage: `url('${backgroundImage}')` }}>
-      {showLogos && (
-        <Logos logoOne={<MakeLogo />} logoTwo={<ReactLogo />} />
-      )}
 
       <h1>{headline}</h1>
 
-      <div className="shape-container">
-        <Shapes />
-      </div>
     </div>
   );
 }
@@ -30,7 +24,7 @@ App.propTypes = {
 App.defaultProps = {
   headline: 'Hello World',
   showLogos: true,
-  backgroundImage: '',
+  backgroundImage: 'http://ruang-ilmiah.oss-ap-southeast-5.aliyuncs.com/template_cert/template-sertifikat-1.png',
 }
 
 export default App;
